216fedd54c9e752e5372eaf6ddc0471632abef03
Biancadesck <bianca.desck@escola.pr.gov.br>
2026-09-24 09:50:30 -0300

    Fix variable names and update symbols
    Corrected spelling of 'letrasMaiuculas' to 'letrasMaiusculas' and removed a symbol from 'simbolos'.

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
-const letrasMaiuculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
 const numeros = '0123456789';
-const simbolos = '!?*@%#'
+const simbolos = '!?*@%';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
